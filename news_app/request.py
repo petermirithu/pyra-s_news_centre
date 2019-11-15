ee4e9cb5cec7d7073c540f8136884308648b1c2b
petermirithu <pyra_m.k@yahoo.com>
2019-11-16 00:11:23 +0300

    add a get source function and process result function for getting and trasnforming the response.

--- news_app/request.py
+++ news_app/request.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from models import source_class
+from news_app.models import source_class
 
 Source=source_class.Source
 
@@ -16,6 +16,39 @@ def get_sources():
   '''
   function that gets a response from the url which sources
   '''
+  source_url=base_url.format(api_key)
+
+  with urllib.request.urlopen(source_url) as url:
+    source_data=url.read()
+    source_response=json.loads(source_data)
+
+    source_results=None
+
+    if source_response['']:
+      source_result_list=source_response['']
+      source_results=process_results(source_result_list)
+
+  return source_results   
+
+def process_results(source_list):
+  '''
+  function that processes the source results to list of objects
+  '''
+  source_results=[]
+  for source_x in source_list:
+    id=source_x.get('id')
+    name=source_x.get('name')
+    language=source_x.get('language')
+    country=source_x.get('country')
+
+    source_object=Source(id,name,language,country)
+    source_results.append(source_object)
+    
+  return source_results  
+
+
+
+
 
 
 
